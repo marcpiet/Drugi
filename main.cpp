831a77a03471a1a03b6400793d1a7667823527a6
marcpiet <mp111@pro.wp.pl>
2017-09-03 23:38:18 +0200

    CPlusPlus version

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <opencv\highgui.h>
 
 void main(void)
@@ -5,4 +6,16 @@ void main(void)
     IplImage *obrazek=cvLoadImage("lena.jpg");
     cvShowImage("okienko",obrazek);
     cvWaitKey();
+}
+*/
+
+#include <opencv2\highgui\highgui.hpp>
+
+using namespace cv;
+
+void main(void)
+{
+    Mat obrazek = imread("lena.jpg");
+    imshow("okienko",obrazek);
+    waitKey();
 }
